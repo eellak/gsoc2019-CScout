a3cca981f9ac7fd00382ac3a36adb51e73de23ec
dimstil <dimstil72@gmail.com>
2019-05-31 23:52:53 +0300

    Http Params passed to functions params as json values

---
+++ src/httpServer.cpp
@@ -0,0 +1,84 @@
+#include "httpServer.h"
+map<utility::string_t, Handler> handler_dictionary;
+
+HttpServer::HttpServer(utility::string_t url) : listener(url){
+    
+    listener.support(methods::GET, std::bind(&HttpServer::handle_get,this, std::placeholders::_1));
+    listener.support(methods::POST, std::bind(&HttpServer::handle_post,this, std::placeholders::_1));
+    listener.support(methods::DEL, std::bind(&HttpServer::handle_delete,this, std::placeholders::_1));
+    listener.support(methods::PUT, std::bind(&HttpServer::handle_put,this, std::placeholders::_1)); 
+    cout << "HttpServer: constructor called listen at "<< url << endl;
+}
+
+void HttpServer::addHandler(utility::string_t value,function <json::value(json::value*)> handleFunction,json::value* attributes){
+    Handler funcHandler;
+    funcHandler.value = value;
+    funcHandler.handleFunction = handleFunction;
+    funcHandler.attributes = attributes;
+    handler_dictionary[value] = funcHandler;
+    //handler_dictionary.insert(pair<utility::string_t,Handler>(value,funcHandler));   
+    cout << "HttpServer: addHandler " << value<< " called \n";
+}
+
+void HttpServer::serve(){
+    http_listener * list = &(this->listener);
+    cout <<"HttpServer: serve begin. Mapped functions\n";
+    for(auto it = handler_dictionary.begin(); it != handler_dictionary.end(); it++){
+        cout << it->first <<" - " << it->second.value << endl;
+    }
+    try{
+        this->listener.open()
+            .then([&list](){cout << "\n Http Rest Server starts listening \n";})
+            .wait();
+        while(true);
+    }
+    catch (exception const & e){
+        cout << e.what() << endl;
+    }
+}
+
+void HttpServer::handle_get(http_request request){
+ /* to add handler */
+
+    utility::string_t path = request.relative_uri().path();
+    cout << "HttpServer: Handle get of "<<path << endl;
+    json::value response;
+
+    cout << "HttpServer: Handle get of "<<path << endl;
+    cout <<"HttpServer: Get begin. Mapped functions\n";
+  
+    auto it = handler_dictionary.find(path.substr(1));
+    if (it == handler_dictionary.end()){
+        //response = json::value(json::object["error"] = (U("Url Not found")));
+        response["error"] = json::value::string("Url Not Found");
+        request.reply(status_codes::NotFound,response);
+    }
+    else{
+        cout<<"HttpServer:handle_get: handler:"<<it->first << endl;
+        std::map<utility::string_t, utility::string_t> attributes = web::uri::split_query(request.request_uri().query());
+        json::value attr;
+
+        for(std::map<utility::string_t, utility::string_t>::iterator it = attributes.begin(); it != attributes.end(); it++){
+            attr[it->first] = json::value::string(it->second);
+        }
+        response = it->second.handleFunction(&attr);
+        request.reply(status_codes::OK, response);
+    }
+}
+    
+
+
+
+void HttpServer::handle_post(http_request request){
+ /* to add handler */
+}
+
+
+void HttpServer::handle_put(http_request request){
+ /* to add handler */
+}
+
+
+void HttpServer::handle_delete(http_request request){
+ /* to add handler */
+}
---
+++ src/httpServer.h
@@ -0,0 +1,35 @@
+#ifndef HTTPSERV_H_
+#define HTTPSERV_H_
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <string>
+#include <functional>
+
+using namespace std;
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+ 
+
+typedef struct{
+    utility::string_t value;
+    function <json::value(json::value*)> handleFunction;
+    json::value* attributes;
+} Handler;
+
+
+class HttpServer{
+
+public:
+    HttpServer(){}
+    HttpServer(utility::string_t url);
+    void handle_get(http_request request);
+    void handle_post(http_request request);
+    void handle_delete(http_request request);
+    void handle_put(http_request request);
+    void addHandler(utility::string_t value,function <json::value(json::value*)> handleFunction,json::value* attributes);
+    void serve();
+    http_listener listener;
+
+};
+#endif
