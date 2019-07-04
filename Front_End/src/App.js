0290afb9ff70b2002ea0e0f35a2effdd3f033c45
dimstil <dimstil72@gmail.com>
2019-07-04 20:18:33 +0300

    Add Side drawer, backdrop dummy main page

--- Front_End/src/App.js
+++ Front_End/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Toolbar from './Components/Toolbar/Toolbar'
-import Filemetrics from './Components/Filemetrics'
+import Toolbar from './Components/Toolbar/Toolbar';
+import Main from './Components/Main';
+import Filemetrics from './Components/Filemetrics';
+import SideDrawer from './Components/Toolbar/SideDrawer';
+import Backdrop from './Components/Toolbar/Backdrop';
 class App extends Component {
   constructor() {
     super();
     this.state =  { 
-      type: "homepage"
+      type: "homepage",
+      sideDrawerOpen: false
     }
   }
 
@@ -16,20 +20,39 @@ class App extends Component {
     });
   };
 
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false});
+  };
 
   render() {
     let to_render;
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler}/>;
+    }
     switch(this.state.type) {
       case("files"):
         to_render = <Filemetrics/>;
+        break;
+      case("homepage"):
+        to_render = <Main/>;
         break;
       default:
         to_render =  <h1>{this.state.type}</h1>
     }
     return (
       <div className="App">
-          <Toolbar changeType={this.setType} style={{zIndex:'20'}} 
-            style={{zIndex:"600"}}/>
+          <SideDrawer changeType={this.setType} show={this.state.sideDrawerOpen}
+            drawerClickHandler={this.drawerToggleClickHandler}/>
+          {backdrop}
+          <Toolbar changeType={this.setType} style={{zIndex:'20'}}  
+            drawerClickHandler={this.drawerToggleClickHandler} style={{zIndex:"600"}}/>
           {to_render}
       </div>
     );
